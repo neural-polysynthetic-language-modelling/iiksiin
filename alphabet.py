9dabc4dc7f3104a6f5dea79dd78eb9c4f65414a6
Lane Schwartz <dowobeha@gmail.com>
2019-08-19 17:22:48 +0000

    Fix

--- alphabet.py
+++ alphabet.py
@@ -42,7 +42,7 @@ def main(name: str, input_source: Iterable[str], output_filename: str, log_filen
     with open(log_filename, "wt") as log:
         print(f"Symbols in alphabet: {alphabet.number_of_symbols()}", file=log)
         print("-----------------------", file=log)
-        print(f"0\t\tThe integer value 0 is reserved to represent any symbol not in the alphabet")
+        print(f"0\t\tThe integer value 0 is reserved to represent any symbol not in the alphabet", file=log)
         for symbol in sorted(iter(alphabet)):
             message=f"{alphabet[symbol]}\t{Alphabet.unicode_info(symbol)}"
             if symbol==alphabet.end_of_morpheme_symbol:
